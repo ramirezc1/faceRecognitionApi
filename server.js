66e923fb23d56f2060006adb8dab7d3a9da9155b
Cristian Ramirez <ramirezcristian81@live.com>
2020-10-27 10:31:05 -0700

    updating database link

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-angular-78974",
-    user: "",
-    password: "",
-    database: "faceRecogitionDB",
+    host: process.env.DATABASE_URL,
+    ssl:true
+
+
   },
 });
 
